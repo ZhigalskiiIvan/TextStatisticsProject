39ac71d76d362e4e492051a1ef7ea39447bb5742
ZhigalskiiIvan <671342i@gmail.com>
2023-03-23 21:15:28 +0300

    create classes skeleton

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,60 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+object StatisticBuilder {
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    fun buildGraphic() {}
+
+    fun printStatisticsInConsole() {}
+
+}
+
+
+object TextReader {
+    fun readFromConsole(): String {
+        TODO()
+    }
+}
+
+
+class TextData() {
+    private val textsList = mutableListOf<Text>()
+
+    fun addNewText(textName: String, content: String) {}
+
+    object TextAnalyzer {
+        private val DELIMITERS = listOf('.', '!', '?')
+
+        fun returnListOfSentences(text: Text): List<Text.Sentence> { TODO() }
+
+    }
+
+    class Text(private val name: String, private val sentencesCount: Int) {
+
+        val sentencesList = listOf<Sentence>()
+
+        fun getSentencesCount() {}
+
+        class Sentence(val words_count: Int) {}
+
+    }
+
+}
+
+
+object CommandCenter {
+
+    enum class Commands(val commandName: String, val executingFun: (Any) -> Any) {
+        Exit("exit", TODO()),
+        Add("add", TODO())
+    }
+
+    fun readCommandFromConsole() {}
+
+    fun executeCommand() {}
+
+}
+
+
+fun main(args: Array<String>) {
+
+    mainCycle@ while (true) {
+    }
 }
